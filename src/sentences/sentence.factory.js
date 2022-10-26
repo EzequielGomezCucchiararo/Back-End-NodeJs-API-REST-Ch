9945ef4bc5555da335da06057ee8a2ba0749e56e
Ezequiel <gomezecu@gmail.com>
2022-10-27 00:37:30 +0200

    test(sentences): sentence factory test

--- src/sentences/sentence.factory.js
+++ src/sentences/sentence.factory.js
@@ -1,5 +1,6 @@
 import crypto from 'crypto';
 
+// TODO: Manage required parameters & errors
 export const sentenceFactory = (description, categories) => {
   const id = crypto.randomUUID();
   const sentence = { id, description, categories };
--- src/sentences/tests/sentence.factory.spec.js
+++ src/sentences/tests/sentence.factory.spec.js
@@ -1,5 +1,20 @@
-describe('My Test Suite', () => {
-  it('My Test Case', () => {
-    expect(true).toEqual(true);
+import crypto from 'crypto';
+import { sentenceFactory } from '../sentence.factory.js';
+
+const id = '1234567';
+const description = 'sentence description A';
+const categories = ['categoryA'];
+
+jest.mock('crypto', () => ({ randomUUID: () => id }));
+
+describe('sentenceFactory', () => {
+  it('should return a valid "sentence" object', () => {
+    const sentence = sentenceFactory(description, categories);
+
+    expect(sentence).toMatchObject({
+      id,
+      description,
+      categories
+    });
   });
 });
